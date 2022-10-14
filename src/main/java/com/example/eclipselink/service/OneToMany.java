86448032b7c1a9a831837cc42155c6bf367caad1
KruKuma <kruthai2540@gmail.com>
2022-10-14 07:41:29 +0200

    Modify class to add One to many relation

--- src/main/java/com/example/eclipselink/service/OneToMany.java
+++ src/main/java/com/example/eclipselink/service/OneToMany.java
@@ -1,5 +1,8 @@
 package com.example.eclipselink.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,43 +18,48 @@ public class OneToMany {
         EntityManager entityManager = emFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        // Create Department Entity
-        Department department = new Department();
-        department.setName("Department");
-
-        // Store Department
-        entityManager.persist(department);
-
         // Create Employee1 Entity
         Employee employee1 = new Employee();
         employee1.setEname("Satish");
         employee1.setSalary(45000.0);
         employee1.setDeg("Technical Writer");
-        employee1.setDepartment(department);
 
         // Create Employee2 Entity
         Employee employee2 = new Employee();
         employee2.setEname("Krisha");
         employee2.setSalary(45000.0);
         employee2.setDeg("Technical Writer");
-        employee2.setDepartment(department);
         
         // Create Employee3 Entity
         Employee employee3 = new Employee();
         employee3.setEname("Masthanvali");
         employee3.setSalary(50000.0);
         employee3.setDeg("Technical Writer");
-        employee3.setDepartment(department);
         
         // Store Employees
         entityManager.persist(employee1);
         entityManager.persist(employee2);
         entityManager.persist(employee3);
         
+        // Create EmployeeList
+        List<Employee> empList = new ArrayList<>();
+        empList.add(employee1);
+        empList.add(employee2);
+        empList.add(employee3);
+        
+        // Create Department Entity
+        Department department = new Department();
+        department.setName("Department");
+        department.setEmployeeList(empList);
+
+        // Store Department
+        entityManager.persist(department);
+
+        
         entityManager.getTransaction().commit();
         entityManager.close();
         emFactory.close();
-
+        
     }
 
 }
