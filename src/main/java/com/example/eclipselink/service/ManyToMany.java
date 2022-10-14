8445c9274cfa5beda75d849f4b8df9701644e386
KruKuma <kruthai2540@gmail.com>
2022-10-14 08:27:51 +0200

    Remove unuse code

--- src/main/java/com/example/eclipselink/service/ManyToMany.java
+++ src/main/java/com/example/eclipselink/service/ManyToMany.java
@@ -15,38 +15,7 @@ public class ManyToMany {
         EntityManager entityManager = emFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        // Create Department Entity
-        Department department = new Department();
-        department.setName("Department");
-
-        // Store Department
-        entityManager.persist(department);
-
-        // Create Employee1 Entity
-        Employee employee1 = new Employee();
-        employee1.setEname("Satish");
-        employee1.setSalary(45000.0);
-        employee1.setDeg("Technical Writer");
-        employee1.setDepartment(department);
-
-        // Create Employee2 Entity
-        Employee employee2 = new Employee();
-        employee2.setEname("Krisha");
-        employee2.setSalary(45000.0);
-        employee2.setDeg("Technical Writer");
-        employee2.setDepartment(department);
         
-        // Create Employee3 Entity
-        Employee employee3 = new Employee();
-        employee3.setEname("Masthanvali");
-        employee3.setSalary(50000.0);
-        employee3.setDeg("Technical Writer");
-        employee3.setDepartment(department);
-        
-        // Store Employees
-        entityManager.persist(employee1);
-        entityManager.persist(employee2);
-        entityManager.persist(employee3);
         
         entityManager.getTransaction().commit();
         entityManager.close();
