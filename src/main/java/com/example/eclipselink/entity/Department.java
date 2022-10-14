472b66500ec37d3a952008fa2f2fe5f1b79923f8
KruKuma <kruthai2540@gmail.com>
2022-10-14 07:29:27 +0200

    Modify department class to accept one to many relation

--- src/main/java/com/example/eclipselink/entity/Department.java
+++ src/main/java/com/example/eclipselink/entity/Department.java
@@ -1,9 +1,12 @@
 package com.example.eclipselink.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Department {
@@ -13,9 +16,20 @@ public class Department {
 
     private int id;
     private String name;
+    
+    @OneToMany(targetEntity = Employee.class)
+    private List employeeList;
 
     public int getId() {
         return id;
+    }
+
+    public List getEmployeeList() {
+        return employeeList;
+    }
+
+    public void setEmployeeList(List employeeList) {
+        this.employeeList = employeeList;
     }
 
     public void setId(int id) {
